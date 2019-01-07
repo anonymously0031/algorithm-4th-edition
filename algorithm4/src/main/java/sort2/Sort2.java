2a18ecd1ab6dc9bb9548ac2d92c781337d276bb4
anonymously0031 <anonymously0031@protonmail.com>
2019-01-07 21:38:41 +0800

    algorithm4

--- algorithm4/src/main/java/sort2/Sort2.java
+++ algorithm4/src/main/java/sort2/Sort2.java
@@ -4,5 +4,6 @@ public class Sort2 {
     public static void main(String[] args) {
       System.out.println("sort2");
       System.out.println("new branch");
+      System.out.println("all branch");
     }
 }
