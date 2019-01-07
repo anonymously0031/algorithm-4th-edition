b33bd2b6a4aa6b27ba7e1976f17eafbf5bf56938
anonymously0031 <anonymously0031@protonmail.com>
2019-01-07 21:19:19 +0800

    algorithm4

--- algorithm4/src/main/java/sort1/Gcd.java
+++ algorithm4/src/main/java/sort1/Gcd.java
@@ -1,5 +1,15 @@
 package sort1;
 
 public class Gcd {
+    public static void main(String[] args) {
+        System.out.println(gcd(13, 16));
+    }
 
+    public static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        int r = p % q;
+        return gcd(q, r);
+    }
 }
