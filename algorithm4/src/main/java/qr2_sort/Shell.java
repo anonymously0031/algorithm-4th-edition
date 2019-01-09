0eae0703f6bfaa153a5e0285d2b9f617d36ab7d7
anonymously0031 <anonymously0031@protonmmail.com>
2019-01-09 10:59:39 +0800

    pom

--- algorithm4/src/main/java/qr2_sort/Shell.java
+++ algorithm4/src/main/java/qr2_sort/Shell.java
@@ -1,6 +1,5 @@
 package qr2_sort;
 
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Shell {
@@ -9,7 +8,7 @@ public class Shell {
 
     public static void main(String[] args) {
 //        String[] a = StdIn.readAllStrings();
-        String[] a = {"12", "ed", "rf", "4r", "y5", "na","m"};
+        String[] a = {"12", "ed", "rf", "4r", "y5", "na", "m"};
         sort(a);
         show(a);
     }
@@ -17,11 +16,14 @@ public class Shell {
     public static void sort(Comparable[] a) { // 将a[]按升序排列
         int N = a.length;
         int h = 1;
-        while (h < N / 3) h = 3 * h + 1; // 1, 4, 13, 40, 121, 364, 1093, ...
+        while (h < N / 3) {
+            h = 3 * h + 1; // 1, 4, 13, 40, 121, 364, 1093, ...
+        }
         while (h >= 1) { // 将数组变为h有序
             for (int i = h; i < N; i++) { // 将a[i]插入到a[i-h], a[i-2*h], a[i-3*h]... 之中
-                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h)
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
                     exch(a, j, j - h);
+                }
             }
             assert isHsorted(a, h);
             h = h / 3;
