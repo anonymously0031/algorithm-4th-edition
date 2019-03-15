f132ae125facf9ec6df297bf981231d8bd51c396
anonymously0031 <anonymously0031@protonmail.com>
2019-03-15 15:08:37 +0800

    优先队列

--- algorithm4/src/main/java/qr2_sort/TopM.java
+++ algorithm4/src/main/java/qr2_sort/TopM.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.Transaction;
 
 public class TopM {
+    //获取最大的M个值
     public static void main(String[] args) {
         int M = Integer.parseInt(args[0]);
         MinPQ<Transaction> pq = new MinPQ<>(M+1);
