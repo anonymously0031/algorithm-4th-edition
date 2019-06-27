048e63d90600c38b37a23a4bb1f2c42f563c2c95
anonymously0031 <anonymously0031@protonmail.com>
2019-06-27 22:08:38 +0800

    无环图

--- algorithm4/src/main/java/qr4_graph/Cycle.java
+++ algorithm4/src/main/java/qr4_graph/Cycle.java
@@ -39,7 +39,7 @@ public class Cycle {
         Graph graph = new Graph(3);
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
-        graph.addEdge(1, 2);
+//        graph.addEdge(1, 2);
         StdOut.println(graph);
         Cycle cycle = new Cycle(graph);
         StdOut.println(graph + "has cycle ? ");
