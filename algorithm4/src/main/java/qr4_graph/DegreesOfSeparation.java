43860aa858b6811bd151a78988c30eb51178937c
anonymously0031 <anonymously0031@protonmail.com>
2019-06-29 19:10:05 +0800

    图中两点间隔的度数

--- algorithm4/src/main/java/qr4_graph/DegreesOfSeparation.java
+++ algorithm4/src/main/java/qr4_graph/DegreesOfSeparation.java
@@ -27,6 +27,8 @@ public class DegreesOfSeparation {
                     for (int v : bfs.pathTo(t)){
                         StdOut.println(" " + sg.name(v));
                     }
+                    Stack<Integer> paths = (Stack<Integer>)bfs.pathTo(t);
+                    StdOut.println((paths.size() - 1) + " Degrees");
                 } else {
                     StdOut.println("Not Connected!");
                 }
