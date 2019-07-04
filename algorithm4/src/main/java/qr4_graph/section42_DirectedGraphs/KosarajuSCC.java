4363a924cdb8be3800a260276297db4277a8570e
anonymously0031 <anonymously0031@protonmail.com>
2019-07-05 00:10:53 +0800

    理解:Kosaraju算法计算有向图的强连通分量

--- algorithm4/src/main/java/qr4_graph/section42_DirectedGraphs/KosarajuSCC.java
+++ algorithm4/src/main/java/qr4_graph/section42_DirectedGraphs/KosarajuSCC.java
@@ -11,6 +11,13 @@ import qr4_graph.section41_UndirectedGraphs.TinyData;
  * 解决“给定的两个顶点是连通的吧？这幅有向图中含有多少个连通分量？”
  * @author leongfeng created on 2017-11-29.
  */
+/**
+ * 核心的思想或许可以总结成两点：
+ * 1.圈反过来也是圈
+ * 2.把圈反一半过来会得到两条路径
+ * 你可以认为第一次搜索之后按倒过来的顺序重新搜，就是为了避开上次走过的路径。
+ * 如果这次能找到另一条路径，和上次那条拼起来就是个圈了。
+ * */
 public class KosarajuSCC {
     private boolean[] marked;
     /**
@@ -23,7 +30,12 @@ public class KosarajuSCC {
         marked = new boolean[G.V()];
         id = new int[G.V()];
         // G 的逆后序排列
+        //反向加边构造成无向图,按照无向图求连通分量的方式进行计算
+        //构造反向图的深度优先搜索排序(第一次深度优先搜索)
         DepthFirstOrder order = new DepthFirstOrder(G.reverse());
+        //所得到的反向图的逆后序-再倒置的顺序,在原图中亦存在这样间隔的深度优先搜索顺序
+        StdOut.println(order.reversePost());
+        //按照反向图的逆后序(拓扑排序)对原图进行深度优先搜索
         for (int s : order.reversePost()) {
             if (!marked[s]) {
                 dfs(G, s);
@@ -57,6 +69,7 @@ public class KosarajuSCC {
     public static void main(String[] args) {
         Digraph G = new Digraph(TinyData.fromFilename("tinyDG.txt"));
         StdOut.println(G);
+        StdOut.println(new DepthFirstOrder(G).reversePost());
         KosarajuSCC cc = new KosarajuSCC(G);
         int M = cc.count();
         StdOut.println(M + " components");
